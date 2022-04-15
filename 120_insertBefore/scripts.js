a873fac21262b0e31aea735c6a04b0a86917e3fd
Daniel <dan_santtos@live.com>
2022-04-15 19:15:45 -0300

    DOM

--- 120_insertBefore/scripts.js
+++ 120_insertBefore/scripts.js
@@ -1,5 +1,7 @@
 let novoElemento = document.createElement("p");
-let elementoAlgo = document.querySelector("#titulo-principal");
+let texto = document.createTextNode("Algum texto");
+novoElemento.appendChild(texto);
+let elementoAlvo = document.querySelector("#titulo-principal");
 let elementoPai = document.querySelector("#container-principal");
 
 elementoPai.insertBefore(novoElemento, elementoAlvo);
